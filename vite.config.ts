2cfdaaabe4462726b0c341255d3714bfc671b17a
diegolsouza <diego@diegosouza.com.br>
2026-01-11 11:46:27 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pages friendly (assets relative) + HashRouter in app.
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: process.env.VITE_BASE ?? "/poker-uplife/",
 });
